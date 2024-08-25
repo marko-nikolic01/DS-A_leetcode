bdc31b1e6bb5dd8e10b01924b4b28dc198982ce9
marko-nikolic01 <nikolicmarko1243@gmail.com>
2024-08-25 14:00:12 +0200

    697. Degree of an Array

---
+++ Problems/697. Degree of an Array/697_degree_of_an_array_cpp/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int findShortestSubArray(vector<int>& nums) {
+    unordered_map<int, pair<int, int>> occurences;
+
+    int shortest = 50000;
+    int degree = 1;
+
+    int n = nums.size();
+    for(int i = 0; i < n; ++i) {
+        if(occurences.find(nums[i]) == occurences.end()) {
+            occurences[nums[i]] = {1, i};
+        } else {
+            occurences[nums[i]].first++;
+        }
+
+        if(occurences[nums[i]].first > degree) {
+            degree = occurences[nums[i]].first;
+            shortest = i - occurences[nums[i]].second + 1;
+        } else if(occurences[nums[i]].first == degree) {
+            int length = i - occurences[nums[i]].second + 1;
+            if(length < shortest) {
+                shortest = length;
+            }
+        }
+    }
+
+    return shortest;
+}
+
+void runTest(vector<int> nums, int expected) {
+    cout << "Input: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Expected: " << expected << endl;
+
+    int result = findShortestSubArray(nums);
+    cout << "Result: " << result << endl;
+    cout << endl;
+}
+
+int main() {
+    runTest({1, 2, 2, 3, 1}, 2);
+    runTest({1, 2, 2, 3, 1, 4, 2}, 6);
+    runTest({1, 1, 1, 2, 2, 2}, 3);
+    runTest({1, 2, 3, 4, 5}, 1);
+    runTest({1, 2, 2, 3, 3, 3, 4}, 3);
+
+    return 0;
+}
