4a1c9262879e2f1be0a69794fb842b0d4b5b071c
Marko Nikolić <116906156+marko-nikolic01@users.noreply.github.com>
2024-10-06 09:58:28 +0200

    116. Populating Next Right Pointers in Each Node

---
+++ Problems/116. Populating Next Right Pointers in Each Node/116_populating_next_right_pointers_in_each_node_cpp/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+Node* connect(Node* root) {
+    if(!root) {
+        return root;
+    }
+
+    queue<Node*> nodes;
+    nodes.push(root);
+
+    while(!nodes.empty()) {
+        if(nodes.front()->left) {
+            for(int i = nodes.size(); i > 1; --i) {
+                Node* current = nodes.front();
+                nodes.push(current->left);
+                nodes.push(current->right);
+                nodes.pop();
+                current->next = nodes.front();
+            }
+            nodes.push(nodes.front()->left);
+            nodes.push(nodes.front()->right);
+            nodes.pop();
+        } else {
+            for(int i = nodes.size(); i > 1; --i) {
+                Node* current = nodes.front();
+                nodes.pop();
+                current->next = nodes.front();
+            }
+            nodes.pop();
+        }
+    }
+
+    return root;
+}
+
+void printTreeLevelByLevel(Node* root) {
+    if (!root) {
+        cout << endl;
+        return;
+    }
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+
+        for (int i = 0; i < levelSize; ++i) {
+            Node* current = q.front();
+            q.pop();
+            cout << current->val << " ";
+
+            if (current->left) q.push(current->left);
+            if (current->right) q.push(current->right);
+        }
+        cout << endl;
+    }
+}
+
+void printTreeLevels(Node* root) {
+    Node* levelStart = root;
+    while (levelStart) {
+        Node* current = levelStart;
+        levelStart = nullptr;
+
+        while (current) {
+            cout << current->val << " ";
+            if (!levelStart && current->left) {
+                levelStart = current->left;
+            }
+            current = current->next;
+        }
+        cout << "# ";
+    }
+    cout << endl;
+}
+
+void runTest(Node* root, const string& expected) {
+    cout << "Tree:" << endl;
+    printTreeLevelByLevel(root);
+
+    cout << "Expected: " << expected << endl;
+
+    connect(root);
+    cout << "Result:   ";
+    printTreeLevels(root);
+
+    cout << endl;
+}
+
+int main() {
+    Node* root1 = new Node(1);
+    root1->left = new Node(2);
+    root1->right = new Node(3);
+    root1->left->left = new Node(4);
+    root1->left->right = new Node(5);
+    root1->right->left = new Node(6);
+    root1->right->right = new Node(7);
+    string expected1 = "1 # 2 3 # 4 5 6 7 # ";
+    runTest(root1, expected1);
+
+    Node* root2 = new Node(1);
+    root2->left = new Node(2);
+    root2->right = new Node(3);
+    string expected2 = "1 # 2 3 # ";
+    runTest(root2, expected2);
+
+    Node* root3 = new Node(1);
+    string expected3 = "1 # ";
+    runTest(root3, expected3);
+
+    Node* root4 = nullptr;
+    string expected4 = "";
+    runTest(root4, expected4);
+
+    return 0;
+}
