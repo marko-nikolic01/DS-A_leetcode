cbd56ae6f2ce0905100c346a426d1d1dfe77ec90
Marko <nikolicmarko1243@gmail.com>
2025-06-24 10:17:16 +0200

    2200. Find All K-Distant Indices in an Array

---
+++ Problems/2200. Find All K-Distant Indices in an Array/2200_find_all_k_distant_indices_in_an_array_cpp/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+    short iIndicies = 0;
+
+    short left = 0;
+    short n = nums.size();
+    for(short right = 0; right < n; ++right) {
+        if(nums[right] == key) {
+            right -= k;
+            while(left < right) {
+                ++left;
+            }
+
+            right += k;
+            while(left < n && left <= right + k) {
+                if(nums[left] == key) {
+                    right = left;
+                }
+
+                nums[iIndicies++] = left++;
+            }
+
+            right += k;
+        }
+    }
+
+    nums.resize(iIndicies);
+
+    return nums;
+}
+
+void printArray(vector<int> array) {
+    for(int a : array) {
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+void test(vector<int> nums, int key, int k, vector<int> expected) {
+    cout << "Array: ";
+    printArray(nums);
+
+    cout << "Key: " << key << endl;
+
+    cout << "k: " << k << endl;
+
+    cout << "Expected: ";
+    printArray(expected);
+
+    cout << "Result: ";
+    printArray(findKDistantIndices(nums, key, k));
+
+    cout << endl;
+}
+
+int main() {
+    test({3, 4, 9, 1, 3, 9, 5}, 9, 1, {1, 2, 3, 4, 5, 6});
+    test({2, 2, 2, 2, 2}, 2, 2, {0, 1, 2, 3, 4});
+    test({1, 2, 3, 4, 5}, 3, 1, {1, 2, 3});
+    test({1, 2, 3, 4, 5}, 1, 2, {0, 1, 2});
+    test({5, 1, 2, 9, 2, 1, 5}, 1, 2, {0, 1, 2, 3, 4, 5, 6});
+
+    return 0;
+}
