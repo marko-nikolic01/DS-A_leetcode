6dd41cbda66ca8d97aef34ad78ff607b73715ee9
marko-nikolic01 <nikolicmarko1243@gmail.com>
2025-11-17 20:00:58 +0100

    1437. Check If All 1's Are at Least Length K Places Away

---
+++ Problems/1437. Check If All 1s Are at Least Length K Places Away/1437_check_if_all_1s_are_at_least_length_k_places_away_cpp/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool kLengthApart(vector<int>& nums, int k) {
+    int previous;
+
+    int i = nums.size();
+    while(--i > -1) {
+        if (nums[i] > 0) {
+            previous = i;
+            break;
+        }
+    }
+
+    while(--i > -1) {
+        if (nums[i] > 0) {
+            if (previous - i <= k) {
+                return false;
+            }
+
+            previous = i;
+        }
+    }
+
+    return true;
+}
+
+void printArray(vector<int> array) {
+    for (int a : array) {
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+void test(vector<int> nums, int k, bool expected) {
+    cout << "Array: ";
+    printArray(nums);
+
+    cout << "k: " << k << endl;
+    cout << "Expected: " << (expected ? "true" : "false") << endl;
+    cout << "Result: " << (kLengthApart(nums, k) ? "true" : "false") << endl;
+
+    cout << endl;
+}
+
+int main() {
+    test({1, 0, 0, 0, 1, 0, 0, 1}, 2, true);
+    test({1, 0, 0, 1, 0, 1}, 2, false);
+    test({0, 1, 0, 0, 1, 0, 0, 1}, 2, true);
+    test({1, 0, 0, 0, 0, 0}, 5, true);
+    test({0, 0, 0, 1, 0, 0, 0, 0}, 3, true);
+    test({1, 1, 0, 0, 0}, 1, false);
+
+    return 0;
+}
