71a1a96b2d6320dccfa26994a61eb701766129f3
marko-nikolic01 <nikolicmarko1243@gmail.com>
2026-01-20 20:05:30 +0100

    3314. Construct the Minimum Bitwise Array I

---
+++ Problems/3314. Construct the Minimum Bitwise Array I/3314_construct_the_minimum_bitwise_array_i_cpp/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> minBitwiseArray(vector<int>& nums) {
+    short answer;
+    short powerOf2;
+
+    for(short i = nums.size() - 1; i > -1; --i) {
+        answer = -1;
+        powerOf2 = 1;
+
+        while((nums[i] & powerOf2) > 0) {
+            answer = nums[i] - powerOf2;
+            powerOf2 <<= 1;
+        }
+
+        nums[i] = answer;
+    }
+
+    return nums;
+}
+
+void printArray(vector<int> array) {
+    for(int a : array) {
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+void test(vector<int> nums, vector<int> expected) {
+    cout << "Array: ";
+    printArray(nums);
+
+    cout << "Expected: ";
+    printArray(expected);
+
+    cout << "Result: ";
+    printArray(minBitwiseArray(nums));
+
+    cout << endl;
+}
+
+int main() {
+    test({2, 3, 5, 7}, {-1, 1, 4, 3});
+    test({11, 13, 31}, {9, 12, 15});
+    test({3, 5, 7, 11}, {1, 4, 3, 9});
+    test({2}, {-1});
+    test({17, 19, 23, 29}, {16, 17, 19, 28});
+
+    return 0;
+}
