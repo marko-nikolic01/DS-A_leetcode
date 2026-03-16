dc032461976e307b0db2f7060748a109bec988c8
marko-nikolic01 <nikolicmarko1243@gmail.com>
2026-03-16 17:19:20 +0100

    5. Longest Palindromic Substring

--- Problems/5. Longest Palindromic Substring/5_longest_palindromic_substring_cpp/main.cpp
+++ Problems/5. Longest Palindromic Substring/5_longest_palindromic_substring_cpp/main.cpp
@@ -2,8 +2,59 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int longestPalindrome(string s, int left, int right) {
+    int n = s.length();
+
+    while(left >= 0 && right < n && s[left] == s[right]) {
+        --left;
+        ++right;
+    }
+
+    return --right - left;
+}
+
+string longestPalindrome(string s) {
+    if (s.empty()) {
+        return "";
+    }
+
+    int start = 0;
+    int end = 0;
+    int odd;
+    int even;
+    int maximumLength;
+
+    int n = s.length();
+    for(int i = 0; i < n; ++i) {
+        odd = longestPalindrome(s, i, i);
+        even = longestPalindrome(s, i, i + 1);
+        maximumLength = max(odd, even);
+
+        if(maximumLength > end - start) {
+            start = i - (maximumLength - 1) / 2;
+            end = i + maximumLength / 2;
+        }
+    }
+
+    return s.substr(start, end - start + 1);
+}
+
+void test(string s, string expected) {
+    cout << "String: " << "\"" << s << "\"" << endl;
+
+    cout << "Expected: " << "\"" << expected << "\"" << endl;
+
+    cout << "Result: " << "\"" << longestPalindrome(s) << "\"" << endl;
+
+    cout << endl;
+}
+
+int main() {
+    test("babad", "aba");
+    test("cbbd", "bb");
+    test("a", "a");
+    test("racecar", "racecar");
+    test("aacabdkacaa", "aca");
+
     return 0;
 }
