56c02e9e57a1d62797258889c281d1e962a00c84
marko-nikolic01 <nikolicmarko1243@gmail.com>
2026-06-17 21:59:53 +0200

    3614. Process String with Special Operations II

---
+++ Problems/3614. Process String with Special Operations II/3614_process_string_with_special_operations_ii_cpp/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+char processStr(string s, long long k) {
+    long long len = 0;
+
+    for(auto c : s) {
+        if(c == '*') {
+            if(len) {
+                --len;
+            }
+        } else if(c == '#') {
+            len <<= 1;
+        } else if (c != '%') {
+            ++len;
+        }
+    }
+
+    if(k + 1 > len) {
+        return '.';
+    }
+
+    for(int i = s.size() - 1; i > -1; --i) {
+        if(s[i] == '*') {
+            ++len;
+        } else if(s[i] == '#') {
+            if(k + 1 > (len + 1) >> 1) {
+                k -= len >> 1;
+            }
+
+            len = (len + 1) >> 1;
+        } else if(s[i] == '%') {
+            k = len - k - 1;
+        } else if (k + 1 == len) {
+            return s[i];
+        } else {
+            --len;
+        }
+    }
+
+    return '.';
+}
+
+void test(string s, long long k, char expected) {
+    cout << "s: " << "\"" << s << "\"" << endl;
+
+    cout << "k: " << k << endl;
+
+    cout << "Expected: " << "'" << expected << "'" << endl;
+
+    cout << "Result: " << "'" << processStr(s, k) << "'" << endl;
+
+    cout << endl;
+}
+
+int main() {
+    test("a#b%*", 1, 'a');
+    test("cd%#*#", 3, 'd');
+    test("z*#", 0, '.');
+    test("ab%%", 0, 'a');
+    test("abc#%", 4, 'b');
+    test("a#####", 31, 'a');
+
+    return 0;
+}
