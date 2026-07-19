8d91c7f8c25262941cdab392b80790d2a4966453
marko-nikolic01 <nikolicmarko1243@gmail.com>
2026-07-19 17:54:36 +0200

    1081. Smallest Subsequence of Distinct Characters

---
+++ Problems/1081. Smallest Subsequence of Distinct Characters/1081_smallest_subsequence_of_distinct_characters_cpp/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string smallestSubsequence(string s) {
+    vector<short> letters(26, 0);
+    int usedLetters = 0;
+
+    short iSubsequence;
+    short n = s.length();
+    for(iSubsequence = 0; iSubsequence < n; ++iSubsequence) {
+        letters[s[iSubsequence] - 'a'] = iSubsequence;
+    }
+
+    iSubsequence = -1;
+    for(short i = 0; i < n; ++i) {
+        if((usedLetters & (1 << (s[i] - 'a'))) < 1) {
+            while(iSubsequence > -1 && s[i] < s[iSubsequence] && letters[s[iSubsequence] - 'a'] > i) {
+                usedLetters ^= 1 << (s[iSubsequence--] - 'a');
+            }
+
+            s[++iSubsequence] = s[i];
+            usedLetters |= 1 << (s[i] - 'a');
+        }
+    }
+
+    s.resize(++iSubsequence);
+
+    return s;
+}
+
+void test(string s, string expected) {
+    cout << "String: " << "\"" << s << "\"" << endl;
+
+    cout << "Expected: " << "\"" << expected << "\"" << endl;
+
+    cout << "Result: " << "\"" << smallestSubsequence(s) << "\"" << endl;
+
+    cout << endl;
+}
+
+int main() {
+    test("bcabc", "abc");
+    test("cbacdcbc", "acdb");
+    test("a", "a");
+    test("cccc", "c");
+    test("abcdef", "abcdef");
+
+    return 0;
+}
