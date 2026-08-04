ad86324c672268cf0c4a7bee1cfddff5a0365591
marko-nikolic01 <nikolicmarko1243@gmail.com>
2026-08-04 21:29:54 +0200

    2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ Problems/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/2996_smallest_missing_integer_greater_than_sequential_prefix_sum_cpp/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int missingInteger(vector<int>& nums) {
+    short n = nums.size();
+    unordered_set<short> numbers(nums.begin(), nums.end());
+    short prefixLength = 1;
+
+    for(short i = 1; i < n; ++i) {
+        if (nums[i] == nums[i - 1] + 1) {
+            ++prefixLength;
+        } else {
+            break;
+        }
+    }
+
+    prefixLength = ((nums[prefixLength - 1] + nums[0]) * prefixLength) >> 1;
+
+    while(numbers.count(prefixLength) > 0) {
+        ++prefixLength;
+    }
+
+    return prefixLength;
+}
+
+void printArray(vector<int> array) {
+    for(int a : array) {
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+void test(vector<int> nums, int expected) {
+    cout << "Array: ";
+    printArray(nums);
+
+    cout << "Expected: " << expected << endl;
+
+    cout << "Result: " << missingInteger(nums) << endl;
+
+    cout << endl;
+}
+
+int main() {
+    test({1, 2, 3, 2, 5}, 6);
+    test({3, 4, 5, 1, 12, 14, 13}, 15);
+    test({5}, 6);
+    test({1, 2, 3, 4, 5}, 15);
+    test({50, 1, 2, 3}, 51);
+    return 0;
+}
